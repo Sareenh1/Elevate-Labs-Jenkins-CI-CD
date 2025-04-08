669b73ee3c3ef0b0bc72a22415854f02cd1301ac
Ubuntu <ubuntu@ip-172-31-15-249.ap-south-1.compute.internal>
2025-04-08 06:34:51 +0000

    Update app message

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from Jenkins CI/CD!'));
+app.get('/', (req, res) => res.send('Hello from Elevate Lab!'));
 app.listen(3000, () => console.log('App running on port 3000'));
